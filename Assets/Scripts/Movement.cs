2aba47fbfb96cfded564c84ca87a87e1fc823e86
Waldek <waldek.bus@gmail.com>
2023-11-18 12:59:01 +0100

    Basic Rocket Movement
    Instead of Transform.Rotate from the course we use RigidBody.AddForceAtPosition using two nested thruster objects

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] int mainThrustPower = 10;
+    [SerializeField] int rotationThrustPower = 10;
+
+    private Rigidbody rb;
+
+    private Transform leftThruster;
+    private Transform rightThruster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        leftThruster = transform.Find("LeftThruster");
+        rightThruster = transform.Find("RightThruster");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrustPower * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Get the position and rotation of the left thruster in world space
+            Vector3 thrusterPosition = rightThruster.position;
+            Quaternion thrusterRotation = rightThruster.rotation;
+
+            // Calculate the force direction in the local Y axis of the thruster
+            Vector3 forceDirection = thrusterRotation * Vector3.up;
+
+            rb.AddForceAtPosition(rotationThrustPower * forceDirection * Time.deltaTime, thrusterPosition);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 thrusterPosition = leftThruster.position;
+            Quaternion thrusterRotation = leftThruster.rotation;
+
+            Vector3 forceDirection = thrusterRotation * Vector3.up;
+
+            rb.AddForceAtPosition(rotationThrustPower * forceDirection * Time.deltaTime, thrusterPosition);
+        }
+    }
+}
