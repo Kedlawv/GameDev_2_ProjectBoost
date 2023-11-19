1aac72f66f3c03fa9d47139aeebfdf64f545a5a3
Waldek <waldek.bus@gmail.com>
2023-11-19 14:27:41 +0100

    Scene Loading Logic

--- Assets/Scripts/CollisonHandler.cs
+++ Assets/Scripts/CollisonHandler.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisonHandler : MonoBehaviour
 {
+
     private void OnCollisionEnter(Collision collision)
     {
         switch(collision.gameObject.tag)
@@ -10,8 +12,33 @@ public class CollisonHandler : MonoBehaviour
                 Debug.Log("Collision with freindly object");
                 break;
             case "Finish":
-                Debug.Log("Collision with finish");
+                LoadNextScene();
                 break;
+            default:
+                ReloadScene();
+                break;
+        }
+    }
+
+    private void ReloadScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void LoadNextScene()
+    {
+        int totalScenes = SceneManager.sceneCountInBuildSettings;
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        bool isLastScene = currentIndex == totalScenes - 1;
+
+        if (isLastScene)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
