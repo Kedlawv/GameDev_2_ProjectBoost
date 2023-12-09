9ba96f6e667dba638e6ffe49eeddf792ae4cac88
Waldek <waldek.bus@gmail.com>
2023-12-09 09:44:20 +0100

    Sine Oscilation Epsilon div by 0

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 
 {
     Vector3 startingPosition;
+    float movementFactor;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +17,18 @@ public class Oscillator : MonoBehaviour
         startingPosition = this.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; } // defend agains division by 0,
+                                                 // use epsilon instead of ==0 because this is two floats
+       
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283 
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2; // recalculated to go from 0 to 1
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
